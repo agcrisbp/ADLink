a729737757b8b41d24f226ee2055b4d0a61e2b78
agcrisbp <agcrisbp@email.com>
2025-01-09 00:24:20 +0700

    Add .env

--- data/BioData.js
+++ data/BioData.js
@@ -2,7 +2,7 @@ const bioData = [
     {
         name: 'Your Name',
         titleImg: false,
-        online: false,
+        online: true,
         url: '#', //redirect when someone click your online status
         avatar: '/favicon.ico', //put your profile photo to public folder
         nftAvatar: true,
@@ -12,17 +12,18 @@ const bioData = [
         subdescShow: false,
         spotify: false, //set to true to show
         spotifyText: 'Recently Played',
+        // Use https://spotify-github-profile.kittinanx.com/api/login
         spotifyUrl: 'FIRST URL',
         spotifyImg: 'SECOND URL',
         bsky: true,
-        bskyUname: 'aghea.biz.id',
+        bskyUname: 'YourHandle',
         bskyTheme: 'light',
         ig: true,
         igUrl: 'https://www.instagram.com/instagram/embed/',
         igPost: true,
         igPostUrl: 'https://www.instagram.com/p/Ct16XZCpv7V/embed',
         tweet: true,
-        tweetUrl: 'https://twitter.com/crisminolog?ref_src=twsrc%5Etfw',
+        tweetUrl: 'https://twitter.com/YourUsername?ref_src=twsrc%5Etfw',
     },
 ];
 
